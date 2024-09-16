02dab80f11cbb5cc23be5ab805d31c208b3ea70a
Anand Yadav <aanand.18jan@gmail.com>
2024-09-17 01:51:02 +0530

    Added

---
+++ SwiftUIPlayground.playground/Pages/ViewBuilder.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+/*:
+ ***
+ @ViewBuilder in SwiftUI is an attribute that allows you to create a function that returns multiple views. It helps create conditional or grouped views in a declarative manner. It acts as a result builder that can handle multiple statements in the body of a SwiftUI view.\
+ When to Use @ViewBuilder\
+ Conditional Views: When you want to conditionally return different views.\
+ Dynamic Content: When you want to generate multiple views in a declarative way, based on some data or conditions.\
+ Reusable Custom Views: You can use @ViewBuilder to create reusable custom views that accept multiple child views.\
+ ***
+ */
+import Foundation
+
+
+//: [Next](@next)
